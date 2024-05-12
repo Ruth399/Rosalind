4e3c49abe3ae561dced57147e86db93edaa1b448
Ruth399 <66483864+Ruth399@users.noreply.github.com>
2024-05-12 02:13:08 +0100

    Rounding on R

--- R/Intro to R/Basics.R
+++ R/Intro to R/Basics.R
@@ -1,2 +1,17 @@
 getwd()
 2+2
+
+#setwd() to change directory of script - e.g., setwd(/desktop/github/rosalind)
+
+round(3.14)
+#?round() - the question mark calls the help tab
+
+args(round)
+#args lets you see the arguments of a function, e.g., round takes 2 arguments
+#these are x, the number, and digits, the no. of decimal places in rounding
+
+round(4.783020712, digits = 1)
+
+#help.search(chisq.test()) - the help.search() function can help finding functions
+
+
