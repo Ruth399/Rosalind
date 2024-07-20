6ffcbf3a0c3213b9f6c4262e2485eab59c6a5e21
Ruth399 <66483864+Ruth399@users.noreply.github.com>
2024-07-20 23:49:06 +0100

    back
    again

--- Bioinformatics Stronghold/ComputingGCcontent.py
+++ Bioinformatics Stronghold/ComputingGCcontent.py
@@ -15,19 +15,19 @@ for nucleotide in Rosalind_6404:
     nucleotides_count_6404[nucleotide] += 1
 
 for nucleotide in ['A', 'C', 'G', 'T']:
-    print(f"Rosalind_6404 {nucleotide} : {nucleotides_count_6404[nucleotide]}")
+    print(f"Rosalind_6404 {nucleotide}: {nucleotides_count_6404[nucleotide]}")
 
 for nucleotide in Rosalind_5959:
     nucleotides_count_6404[nucleotide] += 1
 
 for nucleotide in ['A', 'C', 'G', 'T']:
-    print(f"Rosalind_5959 {nucleotide} : {nucleotides_count_5959[nucleotide]}")
+    print(f"Rosalind_5959 {nucleotide}: {nucleotides_count_5959[nucleotide]}")
 
 for nucleotide in Rosalind_0808:
     nucleotides_count_6404[nucleotide] += 1
 
 for nucleotide in ['A', 'C', 'G', 'T']:
-    print(f"Rosalind_0808 {nucleotide} : {nucleotides_count_0808[nucleotide]}")
+    print(f"Rosalind_0808 {nucleotide}: {nucleotides_count_0808[nucleotide]}")
 
 
 Rosalind_6404 = "CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCCTCCCACTAATAATTCTGAGG"
@@ -38,15 +38,15 @@ nucleotides_count_6404 = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
 nucleotides_count_5959 = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
 nucleotides_count_0808 = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
 
-# Update nucleotide counts for Rosalind_6404
+#Update nucleotide counts for Rosalind_6404
 for nucleotide in Rosalind_6404:
     nucleotides_count_6404[nucleotide] += 1
 
-# Print nucleotide counts for Rosalind_6404
+#Print nucleotide counts for Rosalind_6404
 for nucleotide in ['A', 'C', 'G', 'T']:
     print(f"Rosalind_6404 {nucleotide}: {nucleotides_count_6404[nucleotide]}")
 
-# Update nucleotide counts for Rosalind_5959
+#Update nucleotide counts for Rosalind_5959
 for nucleotide in Rosalind_5959:
     nucleotides_count_5959[nucleotide] += 1
 
@@ -61,3 +61,5 @@ for nucleotide in Rosalind_0808:
 # Print nucleotide counts for Rosalind_0808
 for nucleotide in ['A', 'C', 'G', 'T']:
     print(f"Rosalind_0808 {nucleotide}: {nucleotides_count_0808[nucleotide]}")
+
+print("back again")
